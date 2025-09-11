efea9c3bf53d8c43c8708c916f81bbfba6fd9553
ganzhang2017 <37806207+ganzhang2017@users.noreply.github.com>
2025-09-11 17:57:11 +0100

    Create guide_content.js

---
+++ api/guide_content.js
@@ -0,0 +1,9 @@
+export const GUIDE_TEXT = `
+Global Talent Visa guide excerpts (placeholder)
+
+- Recognition: awards, speaking, publications
+- Innovation: OSS, patents, product launches
+- Impact: revenue/users/metrics
+- Leadership & mentorship
+`;
+
